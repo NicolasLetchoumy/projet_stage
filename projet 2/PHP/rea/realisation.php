60f2174026333ad7ebce93841c87ac1dc7b5f48b
NicolasLetchoumy <84972175+NicolasLetchoumy@users.noreply.github.com>
2021-10-14 09:20:27 +0200

    Update realisation.php

--- projet 2/PHP/rea/realisation.php
+++ projet 2/PHP/rea/realisation.php
@@ -15,6 +15,7 @@
         <h1 class="titre">bienvenue sur le site du formulaire</h1>
     </header>
     <main>
+        <!-- titre récupérer dans l'index du site via le popup -->
         <h1 class="titre">
         <?php
             include('../inc/_titre.php');
@@ -25,6 +26,7 @@
             <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
         </svg>
         <form action="" method="post">   
+
             <!-- titre du formulaire et 1er popup-->
             <?php
                 include('../inc/_premierePopupRea.php');
@@ -36,10 +38,12 @@
             <?php
                 include('../inc/_poppupQuestion.php');
             ?>
+
             <!-- toutes les question rentrer par l'utilisateur question utilisateur -->
             <div id="toutesQuestion">
                    
             </div>
+
             <!--selection des question-->
           <?php
                 include('../inc/_selection.php');
