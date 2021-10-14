557420264a1c8c882b3ae7fcc217ad2fd418b6b6
NicolasLetchoumy <84972175+NicolasLetchoumy@users.noreply.github.com>
2021-10-14 09:18:04 +0200

    Create _poppupQuestion.php

---
+++ projet 2/PHP/inc/_poppupQuestion.php
@@ -0,0 +1,10 @@
+<div id="overlay2">
+                <div id="formulairePartie2">
+                <input type="button" name="" class="fermerPopup2" value="X">
+                    <h3>écrivez votre question</h3>
+                    <div class="input-group input-group-lg">
+                            <input type="text" name="question" class="form-control" id="question" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" value="" placeholder="Question">
+                            <input type="submit" name="validerQuestion" class="btn btn-primary"  id="validerQuestion" value="valider">
+                    </div>
+                </div>
+            </div>       
