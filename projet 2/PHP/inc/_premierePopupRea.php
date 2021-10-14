d2772931ef66039f5513276fae237f1649473864
NicolasLetchoumy <84972175+NicolasLetchoumy@users.noreply.github.com>
2021-10-14 09:18:07 +0200

    Create _premierePopupRea.php

---
+++ projet 2/PHP/inc/_premierePopupRea.php
@@ -0,0 +1,11 @@
+ <!-- titre du formulaire et 1er popup-->
+<div id="overlay">
+                <div id="formulairePartie1">
+                <input type="button" name="" class="fermerPopup" value="X">
+                    <h3>entrez le titre du formulaire</h3>
+                    <div class="input-group input-group-lg">
+                            <input type="text" name="titreFormulaire" class="form-control" id="titreFormulaire" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" value="" placeholder="Titre">
+                            <input type="submit" name="validerFormulaire" class="btn btn-primary"  id="validerFormulaire" value="valider">
+                    </div>
+                </div>       
+            </div>
