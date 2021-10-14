91cc1a54a8331719e96cda318aadb42e60c4964b
NicolasLetchoumy <84972175+NicolasLetchoumy@users.noreply.github.com>
2021-10-14 09:01:24 +0200

    Update index.php

--- projet 2/PHP/index.php
+++ projet 2/PHP/index.php
@@ -13,34 +13,9 @@
         <h1 class="titre">bienvenue sur le site du formulaire</h1>
     </header>
     <main>
-        <section class="modeleFormulaire">
-            <img src="../css/image/form1.jpg" alt="">
-            <img src="../css/image/form2.jpg" alt="">
-            <img src="../css/image/form3.jpg" alt="">
-            <img src="../css/image/form4.jpg" alt="">
-            <img src="../css/image/form5.jpg" alt="">
-            <img src="../css/image/form6.jpg" alt="">
-        </section>
-        <div class="centrage">
-            <p class="presentation">bonjour et bienvenu sur ce site pour réaliser le formulaire de vos rêves parmis un nombre impressionnant de modèle divers et varié.
-                vous pouvez également rajouter des section a votre formulaire si vous en avez le besoin.
-                vous pourrez le partager avec un lien, partager directement sur vos réseaux sociaux ou bien le télecharger et l'imprimer a la fin de la création du formulaire.
-                merci a tous d'être parmis nous
-            </p>
-            <button id="btnAccueil">cliquez ici pour commencer a concevoir votre formulaire</button>
-        </div>
-        <form action="http://localhost/projet%20stage/projet%202/PHP/rea/realisation.php" method="POST" class="formulaire">
-        <div id="overlay">
-            <div id="formulairePartie1">
-                <input type="button" name="" class="fermerPopup" value="X">
-                <h3>entrez votre titre</h3>
-                <div class="input-group input-group-lg">
-                        <input type="text" name="titre" class="form-control" id="titre" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" value="" placeholder="Titre" required>
-                        <input type="submit" class="btn btn-primary"  id="validerAccueil" value="valider">
-                    </div>
-                </div>       
-            </div>
-        </form>
+        <?php
+            include('inc/_mainIndex.php');
+        ?>
     </main>
     <footer>
         <div class="logoReseaux">
