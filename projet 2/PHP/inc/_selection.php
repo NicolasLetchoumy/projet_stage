4a5d962b70b29828f5bd92fade1f3027f78d1305
NicolasLetchoumy <84972175+NicolasLetchoumy@users.noreply.github.com>
2021-10-14 09:18:10 +0200

    Create _selection.php

---
+++ projet 2/PHP/inc/_selection.php
@@ -0,0 +1,10 @@
+<!--selection des question-->
+<div id="formulaireSelect1">
+                <select class="form-select" aria-label="Default select example" id="selected" name="selected">
+                    <option selected="selected" >Sélectionner votre question</option>
+                    <option value="1">Quel âge avez vous?</option>
+                    <option value="2">votre sexe?</option>
+                    <option value="3">écrivez votre question réponse oui ou non</option>
+                    <option value="4">valider le formulaire</option>
+                </select>
+            </div>
